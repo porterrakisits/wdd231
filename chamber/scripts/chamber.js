9d6d1d0d3f0aed2881c95f918d01b3d630030002
porterrakisits <prakisits@byupathway.edu>
2025-03-19 19:27:22 -0600

    began work on Home page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -95,4 +95,40 @@ getBusinessData(url)
 
 
 
+        // weather        
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
+
+
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=172b7656bc684338c07b0c9f48954761';  
+
+
+async function apiFetch() {
+    try {
+      const response = await fetch(weatherUrl);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); 
+        displayWeather(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+
+
+  function displayWeather(data) {
+    currentTemp.innerHTML = `${data.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+    captionDesc.textContent = `${desc}`;
+  }
